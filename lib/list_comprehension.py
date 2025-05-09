88b13a4c371ef04dfd82b481c7014ab41585c533
roriobarnoh <roriokiptoo2016@gmail.com>
2025-05-09 13:44:53 +0300

    my updates

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    list = []
+    for num in num_list:
+        if num % 2 == 0:
+            list.append(num)
+    return list
 
 def make_exclamation(sentence_list):
-    pass
+    my_array = []
+    for sentence in sentence_list:
+        my_array.append(sentence + "!")
+    return my_array
